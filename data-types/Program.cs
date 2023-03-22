182c4332095742bfb81612878bbdec8e1e00ea86
Shawn Mikelson <smikelson75@gmail.com>
2023-03-21 21:22:25 -0500

    Add datatype and hello world examples

---
+++ data-types/Program.cs
@@ -0,0 +1,30 @@
+﻿// Disable unused variable warningh
+#pragma warning disable 219
+
+// Numeric Built-in Value types -> Integer
+//    signed: sbyte (1 byte), short (2 bytes), int (4 bytes), long (8 bytes)
+//    unsigned: byte (1 byte), ushort (2 bytes), uint (4 bytes), ulong (8 bytes)
+// (https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/integral-numeric-types)
+sbyte one_byte = 127;
+
+// Numeric Built-in Value types -> Float
+//     float (4 bytes), double (8 bytes), decimal (16 bytes)
+// (https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/floating-point-numeric-types)
+float four_byte_float = 10.23f;
+double eight_byte_double = 10.1234d;
+decimal sixteen_byte_decimal = 10.123456m;
+
+// Boolean Built-in Value type (bool)
+// (https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/bool)
+bool true_false = true;
+
+// Character Unicode UTF-16 Built-in Value type (char)
+// (https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/char)
+char two_byte_char = 'A';
+
+// Strings
+// (https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/strings/)
+string greeting = "Hello World, ";
+string person = new String("John");
+string personalized_greeting = greeting + person + "!"; // concatenation
+
---
+++ helloworld/Program.cs
@@ -0,0 +1,17 @@
+﻿// Creating a basic console application in .NET 6 Core
+// > dotnet new console --framework net6.0
+//
+// .NET Core 6 introduced a new Program.cs file that simplifies
+// code written for main. If you want to revert to the previous programming
+// style
+// > dotnet new console --use-program-main --framework net6.0
+//
+// To build debug version
+// > dotnet build
+//
+// To build a release version
+// > dotnet build --configuration Release
+//
+// Build and execute executable
+// > dotnet run
+Console.WriteLine("Hello, World!");
